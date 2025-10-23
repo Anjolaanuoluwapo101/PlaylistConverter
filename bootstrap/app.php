ce95ecec9de483451469d967f2df8c1f734040c4
Anjola <anjolaakinsoyinu@gmail.com>
2025-10-23 21:48:25 +0100

    removing postgress

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,8 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
+        $middleware->trustProxies('*');
+
         $middleware->web(append: [
             HandleAppearance::class,
             HandleInertiaRequests::class,
